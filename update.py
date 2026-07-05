6f961841d3b70fd71486707c3d5d3f06d617763f
fifthidea <991121xx@gmail.com>
2026-07-05 06:40:31 +0330

    Update update.py

--- update.py
+++ update.py
@@ -26,9 +26,9 @@ client = TelegramClient(
 # CONFIG (EDIT THIS ONLY)
 # =========================
 CHANNELS = {
-    "ConfigsHUB2": 2400,
-    "TheFreeConfigs": 190,
-    "persianvpnhub": 150,
+    "ConfigsHUB2": 2600,
+    "TheFreeConfigs": 150,
+    "persianvpnhub": 120,
 }
 
 CHANNEL_ACTIVITY_DAYS = 7
