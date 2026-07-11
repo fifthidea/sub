d7f6112ca0d2b12009767d505d46abbd7f40adcf
fifthidea <991121xx@gmail.com>
2026-07-12 01:23:56 +0330

    Update update.py

--- update.py
+++ update.py
@@ -56,6 +56,7 @@ CHANNELS = {
     -1001996629183: {"limit": 2600, "name": "ConfigsHUB2"},
     -1002641485940: {"limit": 150, "name": "TheFreeConfigs"},
     -1003744627358: {"limit": 120, "name": "persianvpnhub"},
+    -1002094972529: {"limit": 750, "name": "FarahVPN" },
 }
 
 CHANNEL_ACTIVITY_DAYS = 3
